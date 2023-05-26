69070586ce829a7daa87ca72495fc21ab09c21ae
Asionsolver <asionsolver274@gmail.com>
2023-05-27 01:58:49 +0600

    isolated custom button

--- script.js
+++ script.js
@@ -1,30 +1,47 @@
 class MyButton extends HTMLElement{
     constructor(){
         super();
+        this.shadow = this.attachShadow({mode: 'open'});
     }
 
     connectedCallback(){
-        const button =`
-            <style>
-            button{
-                background-color: #4CAF50;
-                border: none;
-                color: white;
-                padding: 15px 32px;
-                text-align: center;
-                text-decoration: none;
-                display: inline-block;
-                font-size: 16px;
-                }
-            </style>
-            <button>${this.innerHTML}</button>
-        `;
-        this.innerHTML = button;
+        const button = document.createElement('button');
+        button.innerHTML = this.innerHTML;
+        this.shadow.append(button);
+        this.shadow.append(this.#createStyle());
     }
+
+
 
     attributeChangedCallback(){
         
     }
 
-    disconnectedCallback(){}
+    disconnectedCallback(){
+
+    }
+
+    #createStyle(){
+        const style = document.createElement('style');
+        style.innerText = `
+            button{
+                background-color: #613F75;
+                border: none;
+                color: white;
+                padding: 15px 32px;
+                text-align: center;
+                text-decoration: none;
+                display: flex;
+                justify-content: center;
+                align-items: center;
+                font-size: 16px;
+                margin: 4px 2px;
+                cursor: pointer;
+                border-radius: 5px;
+            }
+        `;
+        return style;
+        }
 }
+
+customElements.define('my-button', MyButton);
